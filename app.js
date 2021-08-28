ded5514752b4b7c7a43c110fe918f430ce343c8a
Szabi Vojtek <szabi.vojtek@gmail.com>
2021-08-28 18:15:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// - Create 2 classes with corespondent constructors(class Book and class UI)
+//     - Book class(used for creating book objects)
+//     - UI class(methods only here: for add the book to the list, delete the book, validate the input show confirmation message boxes)
+// Note: Use arrow functions as much as possible when needed.
